d2e72fc43798fefb40312bf9d0a078fdee059575
oga-sven <31472764+oga-sven@users.noreply.github.com>
2017-09-06 13:13:00 +0200

    responsive-ness fixed!  added right images

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Main page</title>
 <meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body>
 <main>
-<div class="row">
-<div class="item"><img class="itemimg" src="media/item1.png"><p>link1</p></div>
-<div class="item"><img class="itemimg" src="media/item2.jpg"><p>link2</p></div>
-</div><div class="row">
-<div class="item"><img class="itemimg" src="media/item1.png"><p>link1</p></div>
-<div class="item"><img class="itemimg" src="media/item1.png"><p>link1</p></div>
+<div class="item"><img class="itemimg" src="media/pic1.JPG" alt="refresh!">
+  <p>link1</p>
 </div>
-</main> 
+<div class="item"><img class="itemimg" src="media/pic2.jpg" alt="refresh!">
+  <p>link2</p>
+</div>
+<div class="item"><img class="itemimg" src="media/pic3.jpg" alt="refresh!">
+  <p>link2</p>
+</div>
+<div class="item"><img class="itemimg" src="media/pic4.jpg" alt="refresh!">
+<p>link1</p>
+</div>
+</main>
 </body>
 </html>
