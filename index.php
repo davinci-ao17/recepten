1267f744be580bdba9ebd2790b15be94f57167f5
oga-sven <31472764+oga-sven@users.noreply.github.com>
2017-09-06 09:17:14 +0200

    some data, not done

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Main page</title>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+<main>
+<div class="row">
+<div class="item"><img class="itemimg" src="media/item1.png"><p>link1</p></div>
+<div class="item"><img class="itemimg" src="media/item2.jpg"><p>link2</p></div>
+</div><div class="row">
+<div class="item"><img class="itemimg" src="media/item1.png"><p>link1</p></div>
+<div class="item"><img class="itemimg" src="media/item1.png"><p>link1</p></div>
+</div>
+</main> 
+</body>
+</html>
