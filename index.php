3731491d1aa66632fe2c7398c1d57a557d4efaf4
oga-sven <31472764+oga-sven@users.noreply.github.com>
2017-09-12 09:36:04 +0200

    trying to commit with VS

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Main page</title>
-<meta charset="utf-8">
 <link rel="stylesheet" type="text/css" href="css.css">
 </head>
 <body>
-<main>
-<div class="item"><img class="itemimg" src="media/pic1.JPG" alt="refresh!">
-  <p>link1</p>
-</div>
-<div class="item"><img class="itemimg" src="media/pic2.jpg" alt="refresh!">
-  <p>link2</p>
-</div>
-<div class="item"><img class="itemimg" src="media/pic3.jpg" alt="refresh!">
-  <p>link2</p>
-</div>
-<div class="item"><img class="itemimg" src="media/pic4.jpg" alt="refresh!">
-<p>link1</p>
-</div>
-</main>
 </body>
 </html>
