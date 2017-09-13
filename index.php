ce57f0e0d1d942ac83c7c4aef675364e49d39b39
oga-sven <31472764+oga-sven@users.noreply.github.com>
2017-09-13 10:02:50 +0200

    content left only

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="css.css">
-</head>
-<body>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <link rel="shortcut icon" href="media/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="media/favicon.ico" type="image/x-icon">
+        <title>welkom!</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/ecss.css">
+    </head>
+    <body>
+
+    <div id="black">
+    <div id="mainblock">
+    <main>
+            <h1>recepten!</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quaerat molestias sit corrupti ea ex illum nobis, animi dolores sapiente repellat, accusamus officiis vero. Nihil delectus impedit tempore dolores quasi!</p>
+
+            <details>
+            <h1> testtest</h1>
+            <p>Lorem ipsum dolor,
+        sit amet consectetur adipisicing elit. 
+        Illo dignissimos maiores sapiente facere amet 
+        praesentium doloremque sed assumenda distinctio
+         quisquam. Illo unde cupiditate molestiae assumenda,
+          sit accusamus ab doloribus dolores.</p>
+
+          <h2>testest</h2>
+</details>
+    </main>
+    </black>
+    </div>
+    </body>
+    </html>                         
