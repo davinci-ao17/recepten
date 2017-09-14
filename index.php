ba991cd7e1df44221e1364da7f24b4dbeb7a989c
oga-sven <31472764+oga-sven@users.noreply.github.com>
2017-09-14 17:48:14 +0200

    finished assignment

--- index.php
+++ index.php
@@ -1,33 +1,99 @@
-    <!DOCTYPE html>
-    <html>
-    <head>
+<!DOCTYPE html>
+<html>
+<head>
         <link rel="shortcut icon" href="media/favicon.ico" type="image/x-icon">
         <link rel="icon" href="media/favicon.ico" type="image/x-icon">
         <title>welkom!</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/ecss.css">
-    </head>
-    <body>
+        <link rel="stylesheet" type="text/css" href="css/css.css">
+</head>
+<body>
 
-    <div id="black">
     <div id="mainblock">
-    <main>
-            <h1>recepten!</h1>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Et quaerat molestias sit corrupti ea ex illum nobis, animi dolores sapiente repellat, accusamus officiis vero. Nihil delectus impedit tempore dolores quasi!</p>
-
+         <main>
+            <h1>chocoladetaart met banaan!</h1>
+            <p>hier kan je zien hoe je een heerlijke chocoladetaart met banaan kan maken!
+            <br/> lees alles eerst even door en ga dan lekker aan de slag!</p>
             <details>
-            <h1> testtest</h1>
-            <p>Lorem ipsum dolor,
-        sit amet consectetur adipisicing elit. 
-        Illo dignissimos maiores sapiente facere amet 
-        praesentium doloremque sed assumenda distinctio
-         quisquam. Illo unde cupiditate molestiae assumenda,
-          sit accusamus ab doloribus dolores.</p>
-
-          <h2>testest</h2>
-</details>
-    </main>
-    </black>
+                <summary>Ingredienten</summary>
+                <h4>stap 1</h4>
+                <h3>ingredienten</h3>
+                <ul>
+                    <li>170 gram bloem</li>
+                    <li>110 gram boter, gesmolten</li>
+                    <li>koud water (een klein beetje)</li>
+                    <li>1 eetlepel cacaopoeder</li>
+                    <li>2 - 3 bananen (in plakjes)</li>
+                    <li>200 gram pure chocolade (in stukjes)</li>
+                    <li>200 ml slagroom</li>
+                </ul>
+            </details>
+            <details>
+                <summary>voorbereiding</summary>
+                <h4>stap 2</h4>
+                <h3>voorbereiding</h3>
+                <ul>
+                    <li>verwarm de oven op 180 &#8451;</li>
+                    <li>smelt de boter</li>
+                    <li>snij de bananen in plakjes</li>
+                    <li>(in het geval van kopen van een reep)breek de chocola in stukjes</li>
+                </ul>
+            </details>
+            <details>
+                <summary>maak het deeg</summary>
+                <h4>stap 3</h4>
+                <h3>maak het deeg</h3>
+                <ol>
+                    <li>
+                        <ul>
+                            <li>Doe de bloem in een kom en maak er een kuiltje in.</li>
+                            <li>Giet de gesmolten boter in het midden en meng tot het deeg kruimelig wordt. </li>
+                            <li>Voeg cacaopoeder toe en meng goed.</li>
+                            <li>Voeg een beetje water toe en kneed met je hand tot een glad deeg.</li>
+                            <li>Vorm een ​​bal. </li>
+                            <li>leg een schone doek over de kom en laat het deeg 30 minuten rusten op een koele plek.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>Rol het deeg uit en bedek hiermee de bodem en randen van een licht ingevette taartvorm.</li>
+                            <li>Prik gaatjes in het deeg.</li>
+                            <li>dek af met aluminiumfolie of bakpapier en leg er droge bonen of bakbonen in.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>Bak 20 minuten met de steunvulling in de voorverwarmde oven.</li>
+                            <li>Verwijder de steunvulling en laat helemaal afkoelen.</li>
+                        </ul>
+                    </li>
+                </ol>
+            </details>
+            <details>
+                <summary>maak de vulling</summary>
+                <h4>stap 4</h4>
+                <h3>maak de vulling</h3>
+                <ol>
+                    <li>
+                        <ul>
+                            <li>Verwarm de room in een steelpan tot het begint te borrelen.</li>
+                            <li>Haal van het vuur en doe de chocoladestukjes erin. </li>
+                            <li>Dek onmiddellijk af met een deksel en wacht 5 minuten.</li>
+                            <li>Roer met een houten pollepel tot de chocoladeroom glad is..</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <ul>
+                            <li>Verdeel plakjes banaan over de afgekoelde taartbodem.</li>
+                            <li>Schenk hier de chocoladeroom over. </li>
+                            <li> Laat afkoelen tot kamertemperatuur en zet dan in de koelkast tot de vulling stevig is.</li>
+                        </ul>
+                    </li>
+                </ol>
+                <h5>Garneer de taart eventueel met een paar plakjes banaan.</h5>
+            </details>
+            <p> en zo maak je een prachtige, heerlijke chocoladetaart!</p>
+        </main>
     </div>
-    </body>
-    </html>                         
+</body>
+</html>                         
